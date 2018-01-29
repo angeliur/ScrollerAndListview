6ec52cfe64d0615b0dd689a4fab2d522f4ca46cd
hcq <1174064499@qq.com>
2018-01-29 14:42:15 +0800

    多种listview实例

--- app/src/main/java/com/angeliur/scrollerandlistview/listview/ListviewTestActivity.java
+++ app/src/main/java/com/angeliur/scrollerandlistview/listview/ListviewTestActivity.java
@@ -1,8 +1,10 @@
 package com.angeliur.scrollerandlistview.listview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.angeliur.scrollerandlistview.R;
 
@@ -15,5 +17,25 @@ public class ListviewTestActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview_test);
+    }
+
+    public void btnViewHolder(View view) {
+        startActivity(new Intent(this, NotifyTest.class));
+    }
+
+    public void btnChatItem(View view) {
+        startActivity(new Intent(this, ChatItemListViewTest.class));
+    }
+
+    public void btnScrollHideListView(View view) {
+        startActivity(new Intent(this, ScrollHideListViewTest.class));
+    }
+
+    public void btnFlexible(View view) {
+        startActivity(new Intent(this, FlexibleListViewTest.class));
+    }
+
+    public void btnFocus(View view) {
+        startActivity(new Intent(this, FocusListViewTest.class));
     }
 }
